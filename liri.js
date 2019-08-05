805d52b85fe771e56e723b64121aef7e9308b258
Aaron Lanier <alanier.william@gmail.com>
2019-08-05 11:34:37 -0400

    added OMDB response for movie

--- liri.js
+++ liri.js
@@ -37,6 +37,7 @@ function runLiri(appCommand, userInput) {
             console.log("please enter one of the following commands: `concert-this` `spotify-this-song` `movie-this` `do-what-it-says`")
     }
 };
+
 function getSpotify(songName) {
 
     var spotify = new Spotify(keys.spotify);
@@ -50,13 +51,9 @@ function getSpotify(songName) {
         }
 
         console.log("+================================+")
-
         console.log("Artist name: " + data.tracks.items[0].album.artists[0].name + "\r\n");
-
         console.log("Song Name: " + data.tracks.items[0].name + "\r\n");
-
         console.log("Song preview link: " + data.tracks.items[0].href + "\r\n");
-
         console.log("Album: " + data.tracks.items[0].album.name + "\r\n");
 
         var logSong = "======Begin Spotify Log Entry======" + "\nArtist: " + data.tracks.items[0].album.artists[0].name + "\nSong Name: " + data.tracks.items[0].name + "\n Preview Link: " + data.tracks.items[0].href + "\nAlbum Name: " + data.tracks.items[0].album.name + "\n======End Spotify Log Entry======" + "\n";
@@ -65,6 +62,35 @@ function getSpotify(songName) {
             if (err) throw err;
         });
     });
+};
+
+function getOMDB(movie) {
+   
+    if (!movie) {
+        movie = "Mr. Nobody";
+    }
+    var movieQueryUrl = "http://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=trilogy";
+    
+    axios.request(movieQueryUrl).then(
+        function (response) {
+           
+            console.log("=============================");
+            console.log("* Title: " + response.data.Title + "\r\n");
+            console.log("* Year Released: " + response.data.Year + "\r\n");
+            console.log("* IMDB Rating: " + response.data.imdbRating + "\r\n");
+            console.log("* Rotten Tomatoes Rating: " + response.data.Ratings[1].Value + "\r\n");
+            console.log("* Country Where Produced: " + response.data.Country + "\r\n");
+            console.log("* Language: " + response.data.Language + "\r\n");
+            console.log("* Plot: " + response.data.Plot + "\r\n");
+            console.log("* Actors: " + response.data.Actors + "\r\n");
+            
+            
+            var logMovie = "======Begin Movie Log Entry======" + "\nMovie title: " + response.data.Title + "\nYear released: " + response.data.Year + "\nIMDB rating: " + response.data.imdbRating + "\nRotten Tomatoes rating: " + response.data.Ratings[1].Value + "\nCountry where produced: " + response.data.Country + "\nLanguage: " + response.data.Language + "\nPlot: " + response.data.Plot + "\nActors: " + response.data.Actors + "\n======End Movie Log Entry======" + "\n";
+
+            fs.appendFile("log.txt", logMovie, function (err) {
+                if (err) throw err;
+            });
+        });
 };
 
 runLiri(appCommand, userInput);
